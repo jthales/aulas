942769af4ad22a7c52b139da02756aa33035de19
Jthales <thaleshenrique38@live.com>
2019-08-18 15:01:58 -0300

    att

---
+++ src/app/tarefa-atual/tarefa-atual.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tarefa-atual',
+  templateUrl: './tarefa-atual.component.html',
+  styleUrls: ['./tarefa-atual.component.scss']
+})
+export class TarefaAtualComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
